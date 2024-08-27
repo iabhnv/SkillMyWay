918c446109989a41cb6cec6cb5730c346da6b0de
iabhnv <abhinav050505@gmail.com>
2024-08-27 14:21:49 +0530

    add tailwind

--- SkillMyWay-app/src/App.jsx
+++ SkillMyWay-app/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/navbar/Navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello world</h1>
+      <div className='bg-slate-50'>
+      {/* <Navbar/> */}
+      </div>
+      
     </>
   )
 }
